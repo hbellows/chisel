1033dc2fff1d4938f0e5f83955f2ff71fad8ec41
Harper Bellows <harper.bellows@gmail.com>
2018-05-28 13:29:18 -0600

    add paragraph translation functionality test

--- test/paragraph_test.rb
+++ test/paragraph_test.rb
@@ -6,9 +6,9 @@ class ParagraphTest < Minitest::Test
 
   def test_it_can_make_new_html_paragraph
     paragraph = Paragraph.new
-    input = 'This is a paragraph.'
+    input = "Into the peace and safety of a new dark age.\n"
 
-    result = '<p>This is a paragraph.</p>'
+    result = "<p>Into the peace and safety of a new dark age.</p>"
 
     assert_equal result, paragraph.paragraph_p(input)
   end
