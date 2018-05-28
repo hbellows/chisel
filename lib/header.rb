1cc69c7e9c9eb60dcecbf662b97a86ad2e6c39b7
Harper Bellows <harper.bellows@gmail.com>
2018-05-28 16:26:05 -0600

    add find header functionality

--- lib/header.rb
+++ lib/header.rb
@@ -1,8 +1,17 @@
 class Header
 
+  def find_headers(input)
+    if input.include?('#')
+      format_header(input)
+    else
+      input
+    end
+  end
+
   def format_header(input)
-    # require "pry"; binding.pry
     octo_count = input.count('#')
     "<h#{octo_count}>" + input.delete('#').lstrip + "</h#{octo_count}>"
   end
+
+
 end
