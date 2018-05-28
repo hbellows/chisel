e910ae6d4ff19348f222d62c811f59c154f49dba
Harper Bellows <harper.bellows@gmail.com>
2018-05-28 16:53:02 -0600

    refactor paragraph class and tests; update notes

--- notes.md
+++ notes.md
@@ -1,3 +1,13 @@
 file_read.rb: Reads markdown files
+ =>File_read has been changed to Chisel, since that's the class running the
+ program as a whole
+
 markdown_translator: defines methods that translate markdown to HTML
+  =>changed to markdown_converter
+
 file_write: Writes HTML files translated from markdown
+  =>This will likely be wrapped up in the Chisel class files
+
+Header class file sets up the find header and header formatter methods
+
+Paragraph class file sets up the find paragraph and paragraph formatter methods
--- test/paragraph_test.rb
+++ test/paragraph_test.rb
@@ -4,13 +4,22 @@ require './lib/paragraph'
 
 class ParagraphTest < Minitest::Test
 
+  def test_it_can_detect_a_paragraph
+    paragraph = Paragraph.new
+    input = "This is my paragraph, yo.\n"
+
+    result = "<p>This is my paragraph, yo.</p>"
+
+    assert_equal result, paragraph.find_paragraph(input)
+  end
+
   def test_it_can_make_new_html_paragraph
     paragraph = Paragraph.new
     input = "Into the peace and safety of a new dark age.\n"
 
     result = "<p>Into the peace and safety of a new dark age.</p>"
 
-    assert_equal result, paragraph.paragraph_p(input)
+    assert_equal result, paragraph.format_paragraph(input)
   end
 
 end
