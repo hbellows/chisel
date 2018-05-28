24c607193ca69aa245ac243a2557af033246d977
Harper Bellows <harper.bellows@gmail.com>
2018-05-28 15:28:09 -0600

    change test file to match new source file

---
+++ test/markdown_converter_test.rb
@@ -0,0 +1,25 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/markdown_converter'
+
+
+class MarkdownConverterTest < Minitest::Test
+
+  def test_it_can_translate_to_h1_header
+    chisel = Chisel.new
+    input = "# This is an h1 header."
+
+    result = "<h1>This is an h1 header.</h1>"
+
+    assert_equal result, chisel.header.header_h1(input)
+  end
+
+  def test_it_can_translate_paragraph_to_HTML_syntax
+    chisel = Chisel.new
+    input = "Translate me to an HTML paragraph.\n"
+
+    result = "<p>Translate me to an HTML paragraph.</p>"
+
+    assert_equal result, chisel.paragraph.paragraph_p(input)
+  end
+end
