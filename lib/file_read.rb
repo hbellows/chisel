9bdc3948c5af15522e8f0f468de479ee264758b8
Harper Bellows <harper.bellows@gmail.com>
2018-05-28 15:20:59 -0600

    change FileRead class to Chisel class

--- lib/file_read.rb
+++ lib/file_read.rb
@@ -1,8 +1,24 @@
 
 class FileRead
 
-    def markdown_file
-      File.readlines('markdown_file.md')
-    end
     
+      file = ARGV[0]
+      input_file = File.readlines(file)
+      # chisel = Chisel.new
+
+
 end
+
+#
+#    file = ARGV[0]
+#    my_message = File.read(file).strip
+#    # require "pry"; binding.pry
+#    enigma = Enigma.new
+#    e = enigma.encrypt(my_message)
+#
+#    output_file = ARGV[1]
+#    File.write(output_file, e)
+#    puts "Created 'encrypted.txt' with the key #{enigma.key.join.to_i} and date #{enigma.date}"
+# end
+#
+# Encrypt.new
