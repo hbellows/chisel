2f1ff3a9fd7df51518bb817ec2a043f7483ef58f
Harper Bellows <harper.bellows@gmail.com>
2018-05-29 08:05:13 -0600

    WIP fileread functionality

--- lib/chisel.rb
+++ lib/chisel.rb
@@ -1,20 +1,14 @@
+require './lib/markdown_converter'
 
 class Chisel
-
 
       file = ARGV[0]
       input_file = File.readlines(file)
       markdown_converter = MarkdownConverter.new
-      m = markdown_converter.convert(my_input)
+      m = markdown_converter.convert(input_file)
 
 end
 
-#
-#    file = ARGV[0]
-#    my_message = File.read(file).strip
-#    # require "pry"; binding.pry
-#    enigma = Enigma.new
-#    e = enigma.encrypt(my_message)
 #
 #    output_file = ARGV[1]
 #    File.write(output_file, e)
