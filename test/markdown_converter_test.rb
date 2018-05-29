5a866bf736217202f0b1cc4baab53d2002bf2d0b
Harper Bellows <harper.bellows@gmail.com>
2018-05-29 06:51:48 -0600

    add markdown converter tests

--- test/markdown_converter_test.rb
+++ test/markdown_converter_test.rb
@@ -2,24 +2,31 @@ require 'minitest/autorun'
 require 'minitest/pride'
 require './lib/markdown_converter'
 
-
 class MarkdownConverterTest < Minitest::Test
 
   def test_it_can_translate_to_h1_header
-    chisel = Chisel.new
+    markdown_converter = MarkdownConverter.new
     input = "# This is an h1 header."
 
     result = "<h1>This is an h1 header.</h1>"
 
-    assert_equal result, chisel.header.header_h1(input)
+    assert_equal result, markdown_converter.header.find_headers(input)
   end
 
   def test_it_can_translate_paragraph_to_HTML_syntax
-    chisel = Chisel.new
+    markdown_converter = MarkdownConverter.new
     input = "Translate me to an HTML paragraph.\n"
 
     result = "<p>Translate me to an HTML paragraph.</p>"
 
-    assert_equal result, chisel.paragraph.paragraph_p(input)
+    assert_equal result, markdown_converter.paragraph.find_paragraph(input)
+  end
+
+  def test_it_can_translate_a_header_and_paragraph
+    markdown_converter = MarkdownConverter.new
+    input = "## I'm a header.\n" "I'm a paragraph.\n"
+
+    result = "<h2>I'm a header.</h2>" "<p>I'm a paragraph.</p>"
+    assert_equal result, markdown_converter.convert
   end
 end
