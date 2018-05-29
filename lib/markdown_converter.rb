350168761dbdd332234f9698220f7cedad599134
Harper Bellows <harper.bellows@gmail.com>
2018-05-29 06:51:18 -0600

    add functionality for converting files

--- lib/markdown_converter.rb
+++ lib/markdown_converter.rb
@@ -1,12 +1,15 @@
 require './lib/header'
 require './lib/paragraph'
-require './lib/file_read'
 
 class MarkdownConverter
-  attr_reader :header, :paragraph,
+  attr_reader :header, :paragraph
 
   def initialize
     @header = Header.new
     @paragraph = Paragraph.new
+  end
+
+  def convert
+
   end
 end
