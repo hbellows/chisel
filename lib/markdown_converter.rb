203235a12be9061011f4c202bfce5b4b043537c6
Harper Bellows <harper.bellows@gmail.com>
2018-05-29 08:06:19 -0600

    refactor to include find_header and find_paragraph methods, make more sense to put them here

--- lib/markdown_converter.rb
+++ lib/markdown_converter.rb
@@ -7,9 +7,17 @@ class MarkdownConverter
   def initialize
     @header = Header.new
     @paragraph = Paragraph.new
+
   end
 
-  def convert
-
+  def convert(input)
+    x = input.map do |string|
+      string = string.strip
+      if string.include?('#')
+        header.format_header(string)
+      else
+        paragraph.format_paragraph(string)
+      end
+    end.join
   end
 end
