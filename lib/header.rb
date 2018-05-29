c6f6edb491dece345eda954716a48d3d9df372d9
Harper Bellows <harper.bellows@gmail.com>
2018-05-29 08:05:46 -0600

    find_header refactor, included in converter class instead

--- lib/header.rb
+++ lib/header.rb
@@ -1,15 +1,16 @@
 class Header
 
-  def find_headers(input)
-    if input.include?('#')
-      format_header(input)
-    else
-      input
-    end
-  end
-
   def format_header(input)
     octo_count = input.count('#')
-    "<h#{octo_count}>" + input.delete('#').lstrip + "</h#{octo_count}>"
+    "<h#{octo_count}>" + input.delete('#').strip + "</h#{octo_count}>\n"
   end
 end
+
+#REFACTORED OUT:
+# def find_headers(input)
+#   if input.include?('#')
+#     format_header(input)
+#   else
+#     input
+#   end
+# end
