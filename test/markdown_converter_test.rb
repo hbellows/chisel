2685f139802b8808b4bc3159fd0d32e175b5b1a0
Harper Bellows <harper.bellows@gmail.com>
2018-05-29 08:07:52 -0600

    add test to test markdown_converter.convert method

--- test/markdown_converter_test.rb
+++ test/markdown_converter_test.rb
@@ -6,27 +6,29 @@ class MarkdownConverterTest < Minitest::Test
 
   def test_it_can_translate_to_h1_header
     markdown_converter = MarkdownConverter.new
-    input = "# This is an h1 header."
+    input = "# This is an h1 header.\n"
 
-    result = "<h1>This is an h1 header.</h1>"
+    result = "<h1>This is an h1 header.</h1>\n"
 
-    assert_equal result, markdown_converter.header.find_headers(input)
+    assert_equal result, markdown_converter.header.format_header(input)
   end
 
   def test_it_can_translate_paragraph_to_HTML_syntax
+    # skip
     markdown_converter = MarkdownConverter.new
     input = "Translate me to an HTML paragraph.\n"
 
-    result = "<p>Translate me to an HTML paragraph.</p>"
+    result = "<p>Translate me to an HTML paragraph.</p>\n"
 
-    assert_equal result, markdown_converter.paragraph.find_paragraph(input)
+    assert_equal result, markdown_converter.paragraph.format_paragraph(input)
   end
 
   def test_it_can_translate_a_header_and_paragraph
+    # skip
     markdown_converter = MarkdownConverter.new
-    input = "## I'm a header.\n" "I'm a paragraph.\n"
+    input = ["## I'm a header.\n\n", "I'm a paragraph.\n\n"]
 
-    result = "<h2>I'm a header.</h2>" "<p>I'm a paragraph.</p>"
-    assert_equal result, markdown_converter.convert
+    result = "<h2>I'm a header.</h2>\n<p>I'm a paragraph.</p>\n"
+    assert_equal result, markdown_converter.convert(input)
   end
 end
