8977264842caf256eaabea627725c71981b0ed78
Harper Bellows <harper.bellows@gmail.com>
2018-05-29 18:53:13 -0600

    add functionality for emphasis test

--- test/paragraph_test.rb
+++ test/paragraph_test.rb
@@ -13,21 +13,37 @@ class ParagraphTest < Minitest::Test
     assert_equal result, paragraph.find_paragraph(input)
   end
 
-  def test_it_can_detect_no_paragraph_exists
-    paragraph = Paragraph.new
-    input = "### The Madness from the Sea"
-
-    result = "### The Madness from the Sea"
-    assert_equal result, paragraph.find_paragraph(input)
-  end
-
   def test_it_can_make_new_html_paragraph
     paragraph = Paragraph.new
     input = "Into the peace and safety of a new dark age.\n"
 
-    result = "<p>Into the peace and safety of a new dark age.</p>"
+    result = "<p>Into the peace and safety of a new dark age.</p>\n"
 
     assert_equal result, paragraph.format_paragraph(input)
   end
 
+  def test_it_formats_emphasized_words
+    paragraph = Paragraph.new
+    input = "Into the peace and safety of a *new dark age*.\n"
+
+    result = "<p>Into the peace and safety of a <em>new dark age</em>.</p>\n"
+    assert_equal result, paragraph.format_word_emp(input)
+
+  def test_it_formats_stronged_words
+    paragraph = Paragraph.new
+    input = "Into the **peace & safety** of a new dark age.\n"
+
+    result = "<p>Into the <strong>peace &amp; safety</strong> of a new dark age.</p>\n"
+    assert_equal result, paragraph.format_word_str(input)
+  end
 end
+
+
+# REFACTORED OUT:
+# def test_it_can_detect_no_paragraph_exists
+#   paragraph = Paragraph.new
+#   input = "### The Madness from the Sea"
+#
+#   result = "### The Madness from the Sea"
+#   assert_equal result, paragraph.find_paragraph(input)
+# end
