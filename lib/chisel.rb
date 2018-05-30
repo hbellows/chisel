f4d45bc7abd85661f450273193036ba96692a6fe
Harper Bellows <harper.bellows@gmail.com>
2018-05-29 18:50:33 -0600

    WIP

--- lib/chisel.rb
+++ lib/chisel.rb
@@ -6,6 +6,7 @@ class Chisel
       input_file = File.readlines(file)
       markdown_converter = MarkdownConverter.new
       m = markdown_converter.convert(input_file)
+      require "pry"; binding.pry
 
 end
 
