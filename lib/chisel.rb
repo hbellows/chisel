dc579604f5a925c413028026ae04f8ae90530b48
Harper Bellows <harper.bellows@gmail.com>
2018-05-31 07:37:43 -0600

    finalize read/write functionality, add interpolation

--- lib/chisel.rb
+++ lib/chisel.rb
@@ -6,15 +6,11 @@ class Chisel
   input_file = File.readlines(file)
   markdown_converter = MarkdownConverter.new
   m = markdown_converter.convert(input_file)
-  require "pry"; binding.pry
-  # require "pry"; binding.pry
 
+  output_file = ARGV[1]
+  f = File.write(output_file, m)
+  line_count = `wc -l "#{output_file}"`.strip.split(' ')[0].to_i
+  puts "Converted my_input.markdown (#{line_count} lines) to my_output.html (#{line_count} lines)"
 end
 
-#
-#    output_file = ARGV[1]
-#    File.write(output_file, e)
-#    puts "Created 'encrypted.txt' with the key #{enigma.key.join.to_i} and date #{enigma.date}"
-# end
-#
-# Encrypt.new
+#GOOGLE REMINDER: how to count lines in ruby file write
